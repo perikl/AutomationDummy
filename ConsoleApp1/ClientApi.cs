1356d3470dc6557f7bde1f3fd510c2093a766fa3
Alexander Seregin <service@mongo-software.ru>
2019-05-15 18:47:57 +0300

    Init commit

---
+++ ConsoleApp1/ClientApi.cs
@@ -0,0 +1,20 @@
+﻿using RestSharp;
+
+namespace KBCIBack
+{
+    class ClientApi
+    {
+        private static ClientApi _instance;
+        public RestClient Client { get; set; }        
+
+        private ClientApi()
+        {
+            Client = new RestClient("");            
+        }
+
+        public static ClientApi GetInstance()
+        {
+            return _instance ?? (_instance = new ClientApi());
+        }
+    }
+}
